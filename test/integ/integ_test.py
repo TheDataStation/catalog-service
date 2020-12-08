8624194da20ff1b64e21a5d6e5aeaae5ed294a94
YintongMa <myt941126@gmail.com>
2020-12-08 00:43:02 -0600

    add local db .ignore

--- test/integ/integ_test.py
+++ test/integ/integ_test.py
@@ -30,30 +30,11 @@ class TestCatalogService(unittest.TestCase):
                 elif p["tableName"] == "mass_student.csv":
                     cls.cs.insert_profile("WhatProfile", {"asset": "mass_student", "schema": p})
 
-
-        # cls.cs.insert_profile("User", {"name": "admin", "user_type": 1, "item": 1, "schema": {"addr":{"home":"westlake","company":"bank"},"phone":1234567}})
-        # cls.cs.insert_profile("Item", {"version": 2, "timestamp": "2020-09-16", "user": 1})
-        # cls.cs.insert_profile("Item", {"version": 1, "timestamp": "2020-08-16", "user": 1})
-        # cls.cs.insert_profile("WhoProfile", {"item": 1, "asset": 1, "user": 1, "schema": {"family":{"father":"john","momther":"jenny"}}})
-        # cls.cs.insert_profile("WhatProfile", {"item": 2, "asset": 2, "schema": {"paper":{"field": {"name":"db","field":"metadata"}}}})
         pass
 
     def test_get_profile(self):
         pass
-    # @classmethod
-    # def tearDownClass(cls):
-    #     os.remove(test_db_name)
-    #     pass
-    #
-    # def test_get_profile(self):
-    #     #test search_by_keywords
-    #     self.assertEqual({"id": 1,"item": 1, "asset": 1, "user": 1, "schema": {"family":{"father":"john","momther":"jenny"}}},
-    #                      self.cs.search_by_keywords("admin")["WhoProfile"]["1"], "get who profile error")
-    #
-    #     #test json schema search
-    #     self.assertEqual(
-    #         {"id": 1,"item": 2, "asset": 2, "schema": {"paper":{"field": {"name":"db","field":"metadata"}}}},
-    #         self.cs.search_by_keywords("metadata")["WhatProfile"]["1"], "get what profile error")
+
 
 
 if __name__ == '__main__':
